6cf80e99ecffbf06c52eee94446043aed1f1d6cc
JorgeFVS <jorgevieira99@hotmail.com>
2022-05-02 18:22:11 -0300

    Requisições-postman

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -10,24 +10,40 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+  state = {
+    posts: [
+    {
+    nomeUsuario: "paulinha",
+    fotoUsuario: "https://picsum.photos/50/50",
+    fotoPost: "https://picsum.photos/200/150"
+    },
+    {
+    nomeUsuario: "Ana",
+    fotoUsuario: "https://picsum.photos/50/51",
+    fotoPost: "https://picsum.photos/200/151"
+    },
+    {
+    nomeUsuario: "Julia",
+    fotoUsuario: "https://picsum.photos/50/52",
+    fotoPost: "https://picsum.photos/200/152"
+    }
+  ]}
+
   render() {
+    const listaDePosts = this.state.posts.map((pessoa) => {
+      return(
+        <main>
+        <header>
+        <img>src={pessoa.fotoUsuario} alt={'Imagem do usuario'}</img>
+        <p>{pessoa.nomeUsuario}</p>
+        </header>
+        <img>src={pessoa.fotoPost} alt={'Imagem do post'}</img>
+        </main>
+      )
+    })
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'Ana'}
-          fotoUsuario={'https://picsum.photos/50/51'}
-          fotoPost={'https://picsum.photos/200/151'}
-        />
-        <Post
-          nomeUsuario={'Julia'}
-          fotoUsuario={'https://picsum.photos/50/52'}
-          fotoPost={'https://picsum.photos/200/152'}
-        />
+        {listaDePosts}
       </MainContainer>
     );
   }
