1cdce448a05e6d1db92741a59c2191b99fa422cc
JorgeFVS <jorgevieira99@hotmail.com>
2022-06-24 20:47:15 -0300

    Exercicio-dia-5

---
+++ modulo4/Revisão JS para o Back-end/Dia-5/validaInput.js
@@ -0,0 +1,18 @@
+const validaInput = (input) => {
+    const resposta = {
+        isErro: false,
+        errors: []
+    }
+
+    const chaves = Object.keys(input)
+
+    for(let chave of chaves) {
+        if (input[chave] === undefined) {
+            resposta.isErro = true
+            resposta.errors.push(chave)
+        }
+    }
+    return resposta
+}
+
+console.log(validaInput({id: 1, nome: undefined}))
