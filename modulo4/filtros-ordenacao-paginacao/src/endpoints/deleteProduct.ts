8268b1d7ce52ab89f5768d1fd9817a9befcfd624
JorgeFVS <jorgevieira99@hotmail.com>
2022-07-18 20:11:10 -0300

    Exercicios Filtros, Ordenação e Paginação

---
+++ modulo4/filtros-ordenacao-paginacao/src/endpoints/deleteProduct.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import connection from "../database/connection";
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    let errorCode = 400
+    try {
+        const idProduct = req.params.idProduct
+
+        const [Products] = await connection.raw(`
+        SELECT * FROM Products1
+        WHERE id = ${idProduct}
+        `)
+
+        const Product = Products[0]
+
+        if (!Product) {
+            errorCode = 400
+            throw new Error("Id do produto não existe");
+        }
+
+        await connection.raw(`
+        DELETE FROM Products1
+        WHERE id = "${idProduct}";
+        `)
+
+        res.status(200).send({message: "Produto deletado com sucesso"})
+    } catch (error) {
+        res.status(errorCode).send({message: error.message})
+    }
+}
