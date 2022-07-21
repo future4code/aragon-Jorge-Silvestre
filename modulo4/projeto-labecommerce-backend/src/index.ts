21222010b63d83cea0ed43c4cdb9f51d912b5696
JorgeFVS <jorgevieira99@hotmail.com>
2022-07-21 20:05:33 -0300

    Projeto labecommerce backend primeira parte

---
+++ modulo4/projeto-labecommerce-backend/src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { ping } from './endpoints/ping'
+import { createUser } from './endpoints/createUser'
+import { getUsers } from './endpoints/getUsers'
+import { createProduct } from './endpoints/createProduct'
+import { getProducts } from './endpoints/getProducts'
+
+dotenv.config()
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(process.env.PORT || 3003, () => {
+  console.log(`Servidor rodando na porta ${process.env.PORT || 3003}`)
+})
+
+app.get("/ping", ping)
+
+app.post("/users", createUser)
+
+app.get("/users", getUsers)
+
+app.post("/products", createProduct)
+
+app.get("/products", getProducts)
