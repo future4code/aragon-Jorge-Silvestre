2cffb7964db9fd6502cf318cd50c7c3feb691ae0
JorgeFVS <jorgevieira99@hotmail.com>
2022-08-03 19:13:34 -0300

    Projeto cookenu primeira parte

---
+++ modulo5/projeto-cookenu/src/controller/RecipeController.ts
@@ -0,0 +1,94 @@
+import { Request, Response } from "express";
+import { RecipeDatabase } from "../database/RecipeDatabase";
+import { Recipe } from "../models/Recipe";
+import { Authenticator } from "../services/Authenticator";
+import { IdGenerator } from "../services/IdGenerator";
+
+export class RecipeController {
+    public getAllRecipes = async (req: Request, res: Response) => {
+        let errorCode = 400
+        try {
+            const token = req.headers.authorization
+
+            if (!token) {
+                errorCode = 401
+                throw new Error("Token faltando")
+            }
+
+            const authenticator = new Authenticator()
+            const payload = authenticator.getTokenPayload(token)
+
+            if (!payload) {
+                errorCode = 401
+                throw new Error("Token inválido")
+            }
+
+            const recipeDatabase = new RecipeDatabase()
+            const recipesDB = await recipeDatabase.getAllRecipes()
+
+            const recipes = recipesDB.map((recipeDB) => {
+                return new Recipe(
+                    recipeDB.id,
+                    recipeDB.title,
+                    recipeDB.description,
+                    recipeDB.created_at,
+                    recipeDB.updated_at,
+                    recipeDB.creator_id
+                )
+            }) 
+
+            res.status(200).send({ recipes })
+        } catch (error) {
+            res.status(errorCode).send({ message: error.message })
+        }
+    }
+
+    public RegisterNewRecipe = async (req: Request, res: Response) => {
+        let errorCode = 400
+        try {
+            const token = req.headers.authorization
+            const title = req.body.title
+            const description = req.body.description
+            const creatorId = req.params.creatorId
+
+            const authenticator = new Authenticator()
+            const payload = authenticator.getTokenPayload(token)
+
+            if(!payload) {
+                errorCode = 401
+                throw new Error("Token faltando ou inválido");
+            }
+
+            const idGenerator = new IdGenerator()
+            const id = idGenerator.generate()
+
+            const data = new Date()
+            const dia = String(data.getDate()).padStart(2, "0")
+            const mes = String(data.getMonth() + 1).padStart(2, "0")
+            const ano = data.getFullYear()
+
+            const dataAtual = `${ano}/${mes}/${dia}`
+
+            const recipe = new Recipe(
+                id,
+                title,
+                description,
+                new Date(),
+                new Date(),
+                creatorId
+            )
+
+            const recipeDatabase = new RecipeDatabase()
+            await recipeDatabase.createRecipe(recipe)
+
+            res.status(200).send({
+                message: "Receita cadastrada com sucesso",
+                recipe
+            })
+
+
+        } catch (error) {
+            res.status(errorCode).send({ message: error.message })
+        }
+    }
+}
